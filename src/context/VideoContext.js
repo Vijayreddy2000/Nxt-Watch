446564fcf4f09bd01d2b13aac491684a4e80647e
Vijayreddy2000 <vijayreddypatlolla2000@gmail.com>
2022-09-21 13:18:18 +0000

    Nxtwatch

---
+++ src/context/VideoContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const VideoContext = React.createContext({
+  darkTheme: false,
+  toggleTheme: () => {},
+  savedVideos: [],
+  onClickSaveButton: () => {},
+  activeTab: 'Home',
+  changeTab: () => {},
+  addVideo: () => {},
+})
+export default VideoContext
